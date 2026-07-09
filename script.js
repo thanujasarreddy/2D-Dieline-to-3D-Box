e68f4c9127a17b737059ac5f136514819d1992f6
thanujasarreddy <thanujasareddy@gmail.com>
2026-07-09 11:16:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
 
 import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
+const container = document.getElementById("container");
+
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x202020);
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+camera.position.set(0, 2, 8);
