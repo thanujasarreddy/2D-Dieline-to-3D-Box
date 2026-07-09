5e49bda55bb5726ac0b1db32a80fa21d2ca87ee4
thanujasarreddy <thanujasareddy@gmail.com>
2026-07-09 11:11:03 +0530

    Add imports for THREE.js and OrbitControls

---
+++ script.js
@@ -0,0 +1,3 @@
+import * as THREE from 'https://unpkg.com/three@0.165.0/build/three.module.js';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.165.0/examples/jsm/controls/OrbitControls.js';
